46d0c882065064afbbbe98bf9431addb0ea5fb64
Alexandr Voronkin <admin@elasticweb.ru>
2016-04-17 22:00:16 +0300

    format

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var onFinished = require('on-finished')
 
-function logger(start, finish) {
-    if (arguments.length === 1) {
-        finish = start
-        start = null
+function logger (start, finish) {
+  if (arguments.length === 1) {
+    finish = start
+    start = null
+  }
+  return function (req, res, next) {
+    if (typeof start === 'function') {
+      start(req, res)
     }
-    return function (req, res, next) {
-        if (typeof start === 'function') {
-            start(req, res)
-        }
-        onFinished(res, function(err, res) {
-            if (typeof finish === 'function') {
-                finish(err, req, res)
-            }
-        })
-        next()
-    }
+    onFinished(res, function (err, res) {
+      if (typeof finish === 'function') {
+        finish(err, req, res)
+      }
+    })
+    next()
+  }
 }
 
 module.exports = logger
